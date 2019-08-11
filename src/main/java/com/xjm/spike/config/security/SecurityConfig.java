7583c4f2397c20987857712094fe978cbf472a27
1002100655@qq.com <1002100655@qq.com>
2019-08-11 12:12:00 +0800

    1.对静态资源不拦截

--- src/main/java/com/xjm/spike/config/security/SecurityConfig.java
+++ src/main/java/com/xjm/spike/config/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.xjm.spike.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -23,4 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //解决静态资源被拦截的问题
+        web.ignoring().antMatchers("/**");
+    }
 }
