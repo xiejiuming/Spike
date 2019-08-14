2911f0420360eafc3a49955348af8b55968a9cd1
DIY2019001\user <carson.xie@keguanginfo.com>
2019-08-14 14:34:51 +0800

    登录调整

--- src/main/java/com/xjm/spike/config/security/MyAuthenticationFailHander.java
+++ src/main/java/com/xjm/spike/config/security/MyAuthenticationFailHander.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
